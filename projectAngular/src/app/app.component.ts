449e97b96056ff9a3cdc62f7de6782087e12168c
Erick Kogake <erick.kogake@aigroup.local>
2023-01-11 00:23:12 -0300

    project

---
+++ projectAngular/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss', '../styles.scss']
+})
+export class AppComponent implements OnInit {
+
+  result: any;
+  urlToJson = 'assets/resultados.json';
+  results:any;
+  urlToJsons = 'assets/sistema_pontuacao.json'
+  constructor(public http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<any>(this.urlToJson).subscribe(response => {
+      this.result = response;
+    });
+    this.http.get<any>(this.urlToJsons).subscribe(response => {
+      this.results = response;
+    });
+  }
+  title = 'projectAngular';
+}
